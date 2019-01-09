600dce808aaac2408d606c0d7c9ac6b71eb90969
Unknown <10798368@my.uvu.edu>
2019-01-08 21:33:54 -0700

    render texture test, camera displays to plane

---
+++ DGM2640_CharacterDevelopment/Assets/Scripts/DisplayCameraOnObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class DisplayCameraOnObject : MonoBehaviour
+{
+
+    public RenderTexture TargetTexture;
+
+    public void Start()
+    {
+        TargetTexture
+
+}
