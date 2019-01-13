ce1e942e23bee9abef41dcef617d118aae11404e
Unknown <10798368@my.uvu.edu>
2019-01-12 23:00:51 -0700

    character interaction script, DisplayMessage

--- DGM2640_CharacterDevelopment/Assets/Scripts/DisplayMessage.cs
+++ DGM2640_CharacterDevelopment/Assets/Scripts/DisplayMessage.cs
@@ -1,9 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class DisplayMessage : MonoBehaviour 
+public class DisplayMessage : MonoBehaviour
 {
+    public Text Message;
+    public StringData MyMessage;
+    
+    public void OnTriggerEnter (Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Message.text = MyMessage.output;
+        }
 
+    }
 
 }
