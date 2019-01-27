a922876689d67dff49257d770592333dbff4a7c3
Unknown <10798368@my.uvu.edu>
2019-01-26 22:44:50 -0700

    scene objective changed
    Simon > Color Guess game

---
+++ DGM2640_CharacterDevelopment/Assets/Scripts/SelectRandomID.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectRandomID : MonoBehaviour
+{
+    public NameID Selection;
+    public NameID[] Options = new NameID[0];
+
+    public void Start()
+    {
+        NameID Selection = Options[Random.Range(0, Options.Length)];
+    }
+}
