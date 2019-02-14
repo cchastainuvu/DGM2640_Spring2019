0eb102eb3972ca18e996057e480ed122b90ccc32
Unknown <10798368@my.uvu.edu>
2019-02-14 12:50:17 -0700

    simplified character controller tutorial

---
+++ DGM2640_CharacterDevelopment/Assets/Scripts/Tools/Flip.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flip : MonoBehaviour
+{
+    private Vector3 _rot;
+    
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            _rot.y = 180F;
+            transform.rotation = Quaternion.Euler(_rot);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.rotation = Quaternion.identity;
+            //Identity will set the Quaternion to 0.
+        }
+    }
+
+}
---
+++ DGM2640_CharacterDevelopment/Assets/Scripts/Tools/MoveCharacter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCharacter : MonoBehaviour
+{
+
+	public float MoveSpeed = 5F, Gravity = -9.81F;
+	private CharacterController _cc;
+	private Vector3 _position;
+	
+	void Start ()
+	{
+		_cc = GetComponent<CharacterController>();
+	}
+	
+	void Update ()
+	{
+		_position.z = Input.GetAxis("Horizontal") * MoveSpeed * Time.deltaTime;
+		_position.y = Gravity * Time.deltaTime;
+		_cc.Move(_position);
+	}
+}
