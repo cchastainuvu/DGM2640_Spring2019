e37a993b8789c40bbd8cb032c3d4ad1918ff1991
Unknown <10798368@my.uvu.edu>
2019-02-23 00:34:41 -0700

    count up and count down by seconds script

---
+++ DGM2640_CharacterDevelopment/Assets/Scripts/CountDownBySeconds.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+
+//References: Inverse of CountUpBySeconds.
+//5 minutes of work/troubleshooting.
+public class CountDownBySeconds : MonoBehaviour 
+{
+
+    public Text CountDisplay;
+
+    private float _number = 0F;
+    public FloatData StartCount;
+
+    public bool CanRun { get; set; }
+    private WaitForSeconds _seconds;
+
+
+    private void Start()
+    {
+        _number = StartCount.value;
+        CountDisplay.text = _number.ToString(CultureInfo.InvariantCulture);
+    }
+
+    private IEnumerator RunCoroutine()
+    {
+        _seconds = new WaitForSeconds(1F);
+        yield return _seconds;
+        
+        while (CanRun)
+        {
+            yield return _seconds;
+
+            CountDisplay.text = _number--.ToString(CultureInfo.InvariantCulture);
+        }
+    }
+    
+    public void BeginCounting()
+    {
+        CanRun = true;
+        StartCoroutine(RunCoroutine());
+    }
+    
+    public void StopCounting()
+    {
+        CanRun = false;
+        StopCoroutine(RunCoroutine());
+    }
+}
