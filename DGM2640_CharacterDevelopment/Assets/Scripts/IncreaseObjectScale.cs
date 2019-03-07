c386df6e1c4d9e21018f1c312707d298a12e0682
Unknown <10798368@my.uvu.edu>
2019-03-06 23:50:50 -0700

    increase object scale "balloon" script

---
+++ DGM2640_CharacterDevelopment/Assets/Scripts/IncreaseObjectScale.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//Resources: Unity Scripting API
+//5 minutes of work/troubleshooting
+public class IncreaseObjectScale : MonoBehaviour
+{
+
+    public FloatData TransformX, TransformY, TransformZ;
+
+    public void ScaleUp()
+    {
+        transform.localScale += new Vector3(TransformX.value, TransformY.value, TransformZ.value);
+    }
+
+}
