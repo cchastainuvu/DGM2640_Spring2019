82edf52aabbde2028b2a130c71d8a0d4c1f83660
Unknown <10798368@my.uvu.edu>
2019-03-14 16:51:24 -0600

    toggle activity script

---
+++ DGM2640_CharacterDevelopment/Assets/Scripts/ToggleActivity.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleActivity : MonoBehaviour
+{
+    public GameObject Obj;
+    
+    public void BecomeActive()
+    {
+        Obj.SetActive(true);
+    }
+
+    public void BecomeInactive()
+    {
+        Obj.SetActive(false);
+    }
+}
