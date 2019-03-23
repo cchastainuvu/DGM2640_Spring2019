d6291f8d7f790f65530ed83e54d6a1f9fd8ad8f4
Unknown <10798368@my.uvu.edu>
2019-03-23 01:54:53 -0600

    pong controller, sprite reorganization

---
+++ DGM2640_CharacterDevelopment/Assets/Scripts/PongController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PongController : MonoBehaviour
+{
+
+	private CharacterController _cc;
+	public FloatData MoveSpeed;
+
+	public float PosLimit, NegLimit;
+	
+	private Vector3 _pos;
+
+	void Start()
+	{
+		_cc = GetComponent<CharacterController>();
+	}
+
+
+	void Update()
+	{
+		if (Input.GetAxis("Vertical") > 0F || Input.GetAxis("Vertical") < 0F)
+		{
+			_pos.y = Input.GetAxis("Vertical") * MoveSpeed.Value * Time.deltaTime;
+			_cc.Move(_pos);
+		}
+	}
+}
